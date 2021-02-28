e8ef79f292abfe15850b28788807429dee618e8c
divy3011 <divyias3011@gmail.com>
2021-02-28 20:54:09 +0530

    dashboard bangaya

--- dashboard_homepage_temperarory/Dashboard/js/app.js
+++ dashboard_homepage_temperarory/Dashboard/js/app.js
@@ -1,11 +1,10 @@
 // function set_class(id){
 //   document.getElementById(id).className += " active";
 // }
-// document.getElementById("first").addEventListener("click", function() {
-//   if ( document.getElementById("first").classList.contains('active') ){
-//     document.getElementById("first").classList.remove('active');
-//   }
-//   else{
-//       document.getElementById("first").classList.add('active');
-//   }
-// });
+document.getElementById("first").addEventListener("click", function() {
+  document.getElementById("first").classList.add('active');
+  $(".sidebar").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+});
