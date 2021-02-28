3429f3c50947aa6686b7464d0ea349151e2d27fe
divy3011 <divyias3011@gmail.com>
2021-02-28 17:16:53 +0530

    temperarory layout of dashboard and homepage is ready, search functionality and caraousel to be implemented

---
+++ dashboard_homepage_temperarory/Dashboard/js/app.js
@@ -0,0 +1,11 @@
+// function set_class(id){
+//   document.getElementById(id).className += " active";
+// }
+// document.getElementById("first").addEventListener("click", function() {
+//   if ( document.getElementById("first").classList.contains('active') ){
+//     document.getElementById("first").classList.remove('active');
+//   }
+//   else{
+//       document.getElementById("first").classList.add('active');
+//   }
+// });
