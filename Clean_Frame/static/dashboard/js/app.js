cf401baf25e22282fc36a7aa30b7e7d1a632020e
ABans1 <iit2019218@iiita.ac.in>
2021-03-01 11:00:21 +0530

    http -> https

--- Clean_Frame/static/dashboard/js/app.js
+++ Clean_Frame/static/dashboard/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // function set_class(id){
 //   document.getElementById(id).className += " active";
 // }
@@ -9,15 +8,3 @@ document.getElementById("first").addEventListener("click", function() {
         $("body").toggleClass("sb-sidenav-toggled");
     });
 });
-=======
-// function set_class(id){
-//   document.getElementById(id).className += " active";
-// }
-document.getElementById("first").addEventListener("click", function() {
-  document.getElementById("first").classList.add('active');
-  $(".sidebar").on("click", function(e) {
-        e.preventDefault();
-        $("body").toggleClass("sb-sidenav-toggled");
-    });
-});
->>>>>>> 39f439f2208e050483962b024e98da5d42e38482
