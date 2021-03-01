206298dccddc72cf1e6010117187fd4c65c8abda
divy3011 <divyias3011@gmail.com>
2021-03-01 16:17:52 +0530

    responsiveness for media query size above 768 px and less then 960px resolved, now sidebar shows completely

--- temp/Clean_Frame/home/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import StudentProfile, CompanyProfile
-# Register your models here.
-admin.site.register(StudentProfile)
-admin.site.register(CompanyProfile)
