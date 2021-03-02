c8dd2da4e0e67a681b64632d29f5564dddf633a4
divy3011 <divyias3011@gmail.com>
2021-03-03 00:37:06 +0530

    Profile styling done except for styling it in the table form and profile picture of the corresponding user updated

--- Clean_Frame/dashboard/models.py
+++ Clean_Frame/dashboard/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-class StaffPermissions(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    can_access_student_inactive_accounts=models.BooleanField(default=False)
-    can_access_company_inactive_accounts=models.BooleanField(default=False)
-
-    def __str__(self):
-        if self.user:
-            return self.user.username
-        else:
-            return 'NILL'
