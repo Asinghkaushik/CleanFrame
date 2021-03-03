64a65fe81f992500d20d67c319a4499df9026c6d
ABans1 <iit2019218@iiita.ac.in>
2021-03-03 20:14:36 +0530

    HOME PAGE ERROR ++, SEETINGS APP ADDED, DELETE ACCOUNT FEATURE ADDED, DASHBAORD LEFT PANEL ALIGNMENT CORRECTED

--- Clean_Frame/Clean_Frame/urls.py
+++ Clean_Frame/Clean_Frame/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     path('dashboard/',include('dashboard.urls')),
+    path('settings/',include('Settings.urls')),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
