51ec3585cc3ad1aa78de6648298b86c004e8d3a5
ABans1 <iit2019218@iiita.ac.in>
2021-03-03 22:15:15 +0530

    HOME PAGE ERRORS RESOLVED

--- Clean_Frame/static/home/js/app.js
+++ Clean_Frame/static/home/js/app.js
@@ -2,8 +2,3 @@ const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 tl.to(".text",{y:"0%",duration:1,stagger: 0.25});
 tl.to(".slider",{y:"-100%", duration:1.5,delay:0.5});
 tl.to(".intro",{y:"-100%",duration:1},"-=1");
-
-function menuToggle(){
-  const togglemenu = document.querySelector('.menu');
-  togglemenu.classList.toggle('active');
-}
