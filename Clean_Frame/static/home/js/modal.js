461be19f0efea8442fcc87c6b9554d46aad7759a
Asinghkaushik <ambikasinghkoushik@gmail.com>
2021-03-27 22:23:34 +0530

    Added the blog modal, onclicking read more a modal will open

---
+++ Clean_Frame/static/home/js/modal.js
@@ -0,0 +1,4 @@
+function open_description(blog_id){
+    document.getElementById("blog_description").innerHTML=document.getElementById(blog_id).innerHTML
+    document.getElementById('blog').style.display='block'
+}
